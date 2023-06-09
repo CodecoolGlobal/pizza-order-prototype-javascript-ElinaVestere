a7ae0d4920bf6b025026837cb37d02c525abdc91
Shcoobz <a.boeckmann23@gmail.com>
2023-06-09 17:18:37 +0200

    finished task 3 & 4

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const fs = require(`fs`);
 const path = require('path');
 const pizzaListPath = './data/pizzas.json';
 const allergensListPath = './data/allergens.json';
+const ordersListPath = './data/orders.json';
 const app = express();
 const port = 3000;
 
-let pizzas, allergens;
+let pizzas, allergens, orders;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // app.get('/', (req, res) => {
@@ -29,13 +31,23 @@ fs.readFile(pizzaListPath, 'utf8', (err, data) => {
 });
 
 fs.readFile(allergensListPath, 'utf8', (err, data) => {
-  console.log('\nreading Allergenes List');
+  console.log('\nreading Allergens List');
 
   if (err) {
     console.error('Error reading file:', err);
     return res.status(500).send(err);
   }
   allergens = JSON.parse(data);
+});
+
+fs.readFile(ordersListPath, 'utf8', (err, data) => {
+  console.log('\nreading Orders List');
+
+  if (err) {
+    console.error('Error reading file:', err);
+    return res.status(500).send(err);
+  }
+  orders = JSON.parse(data);
 });
 
 app.get('/api/pizza', (req, res) => {
@@ -50,8 +62,8 @@ app.get('/api/allergen', (req, res) => {
   res.json(allergens);
 });
 
-app.get('/api/pizza/allergens', (req, res) => {
-  console.log('GET at /api/pizza/allergens');
+app.get('/pizza/list', (req, res) => {
+  console.log('GET at /pizza/list');
 
   // find allergens
   const getAllergensForPizza = (pizza) => {
@@ -69,6 +81,58 @@ app.get('/api/pizza/allergens', (req, res) => {
   });
 
   res.json(pizzasWithAllergens);
+});
+
+app.get('/api/order', (req, res) => {
+  console.log('GET at /api/order');
+
+  res.json(orders);
+});
+
+app.post('/api/order', (req, res) => {
+  console.log('POST at /api/order');
+
+  let receivedOrder = req.body;
+
+  // simple id generation for new order
+  let maxOrderId = 0;
+  if (orders.length > 0) {
+    const orderIds = orders.map((order) => order.id);
+    maxOrderId = Math.max(...orderIds);
+  }
+  receivedOrder.id = maxOrderId + 1 || 1;
+
+  // add date to the order
+  const currentDate = new Date();
+  receivedOrder.date = {
+    year: currentDate.getFullYear(),
+    month: currentDate.getMonth() + 1, // months are 0-based in JS
+    day: currentDate.getDate(),
+    hour: currentDate.getHours(),
+    minute: currentDate.getMinutes(),
+  };
+
+  // Construct the new order object in the desired order
+  let newOrder = {
+    id: receivedOrder.id,
+    pizzas: receivedOrder.pizzas,
+    date: receivedOrder.date,
+    customer: receivedOrder.customer,
+  };
+
+  // push new order into orders list
+  orders.push(newOrder);
+
+  // write the new orders list to the file
+  fs.writeFile(ordersListPath, JSON.stringify(orders, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return res.status(500).send(err);
+    }
+
+    // if everything is okay, send the new order back to the client
+    res.json(receivedOrder);
+  });
 });
 
 app.listen(port, () => {
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,6 +1,7 @@
 let pizzaListDiv = document.getElementById('pizza');
 let filterForm = document.getElementById('filterForm');
 let allergenFilter = document.getElementById('allergenFilter');
+let order = [];
 
 async function fetchData(url) {
   const response = await fetch(url);
@@ -61,6 +62,9 @@ function createPizzaDiv(pizzas) {
       <div id="allergens" class="w3-text-grey">Allergens: ${pizza.allergens
         .map((allergen) => allergen.name)
         .join(', ')}</div>
+        <label for="amount${pizza.id}">Amount:</label>
+      <input id="amount${pizza.id}" type="number" min="1">
+      <button onclick="addToOrder(${pizza.id})">Add to order</button>
       <hr />
     `;
     pizzaListDiv.appendChild(pizzaDiv);
@@ -68,9 +72,136 @@ function createPizzaDiv(pizzas) {
 }
 
 async function fetchAndDisplayPizzas() {
-  let pizzas = await fetchData('http://localhost:3000/api/pizza/allergens');
+  let pizzas = await fetchData('http://localhost:3000/pizza/list');
   pizzas = filterPizzas(pizzas);
   createPizzaDiv(pizzas);
+}
+
+// orders
+function addToOrder(pizzaId) {
+  let amountInput = document.getElementById(`amount${pizzaId}`);
+  let amount = Number(amountInput.value);
+
+  if (isNaN(amount) || amount < 1) {
+    alert('Invalid amount');
+    return;
+  }
+
+  // check if pizza already in order and update amount if it is
+  let existingPizzaOrder = order.find((po) => po.id === pizzaId);
+  if (existingPizzaOrder) {
+    existingPizzaOrder.amount += amount;
+  } else {
+    // add pizza to order if it wasn't there before
+    order.push({ id: pizzaId, amount });
+  }
+
+  amountInput.value = '';
+  displayOrderForm();
+}
+
+async function displayOrderForm() {
+  let orderFormDiv = document.getElementById('orderForm');
+  let orderSummaryDiv = document.getElementById('orderSummary');
+
+  if (order.length > 0) {
+    orderFormDiv.style.display = 'block';
+
+    // Get the list of pizzas to get their names
+    let pizzas = await fetchData('http://localhost:3000/pizza/list');
+
+    // Reset the order summary
+    orderSummaryDiv.innerHTML = '';
+
+    let overallTotal = 0;
+
+    // Add a line for each pizza in the order
+    order.forEach((pizzaOrder, index) => {
+      let pizza = pizzas.find((pizza) => pizza.id === pizzaOrder.id);
+      let totalPizzaPrice = pizza.price * pizzaOrder.amount;
+      overallTotal += totalPizzaPrice;
+
+      let pizzaLine = document.createElement('div');
+
+      let pizzaText = document.createElement('p');
+      pizzaText.textContent = `${pizza.name}: ${pizzaOrder.amount}, Total for this pizza: ${totalPizzaPrice}`;
+      pizzaLine.appendChild(pizzaText);
+
+      let decreaseButton = document.createElement('button');
+      decreaseButton.textContent = '-';
+      decreaseButton.addEventListener('click', () => {
+        pizzaOrder.amount--;
+        displayOrderForm(); // Refresh the form
+      });
+      pizzaLine.appendChild(decreaseButton);
+
+      let increaseButton = document.createElement('button');
+      increaseButton.textContent = '+';
+      increaseButton.addEventListener('click', () => {
+        pizzaOrder.amount++;
+        displayOrderForm(); // Refresh the form
+      });
+      pizzaLine.appendChild(increaseButton);
+
+      let deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        order.splice(index, 1); // Remove this pizza from the order
+        displayOrderForm(); // Refresh the form
+      });
+      pizzaLine.appendChild(deleteButton);
+
+      orderSummaryDiv.appendChild(pizzaLine);
+    });
+
+    // display the overall total cost
+    let totalLine = document.createElement('p');
+    totalLine.textContent = `Overall total: ${overallTotal}`;
+    orderSummaryDiv.appendChild(totalLine);
+  } else {
+    orderFormDiv.style.display = 'none';
+  }
+}
+
+async function submitOrder() {
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let city = document.getElementById('city').value;
+  let street = document.getElementById('street').value;
+
+  if (!name || !email || !city || !street) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  let orderDetails = {
+    pizzas: order,
+    customer: {
+      name,
+      email,
+      address: {
+        city,
+        street,
+      },
+    },
+  };
+
+  let response = await fetch('http://localhost:3000/api/order', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderDetails),
+  });
+
+  if (response.ok) {
+    order = [];
+    alert('Order placed successfully');
+  } else {
+    alert('Failed to place order');
+  }
+
+  displayOrderForm();
 }
 
 // main
